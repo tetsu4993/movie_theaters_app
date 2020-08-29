1639f3a328cfa6d23ee0d0a382e5304aa7c62ef9
tetsu4993 <tetsu19901103@gmail.com>
2020-08-30 02:14:41 +0900

    いろいろお直し

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  with_options presence: true do
+    validates :nickname, format: {with: //, message: ""}
+    validates :birthday, format: {with: //, message: ""}
+  end
+  
 end
---
+++ test/controllers/thaters_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class ThatersControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
